9e5c3e609d8f4791c82a30693de1675d9463cd4d
Mafaaz <Mafaaz@LAPTOP-4T7TNPG8>
2023-07-10 09:57:43 +0530

    Commit

---
+++ C13/src/com/cg/dayone/Emojis.java
@@ -0,0 +1,28 @@
+package com.cg.dayone;
+
+public class Emojis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		        System.out.println("\uD83D\uDE00"); // Grinning face
+		        System.out.println("\uD83D\uDE1C"); // Cool face
+		        System.out.println("\uD83D\uDE04"); // Smiling face with open mouth and smiling eyes
+		        System.out.println("\uD83D\uDE2D"); // Loudly crying face
+		        System.out.println("\uD83D\uDC68\u200D\uD83D\uDCBB"); // Man and woman holding hands
+		        System.out.println("\uD83C\uDFB8"); // Musical notes
+		        System.out.println("\uD83C\uDFC8"); // Trophy
+		        System.out.println("\uD83C\uDF89"); // Party popper
+		        System.out.println("\uD83D\uDCBB"); // Personal computer
+		        System.out.println("\uD83C\uDFE0"); // House with garden
+
+		        System.out.println("\uD83D\uDE03"); // Smiling face with open mouth
+		        System.out.println("\uD83D\uDC4D"); // Thumbs up
+		        System.out.println("\uD83D\uDE0E"); // Smiling face with sunglasses
+		        System.out.println("\uD83D\uDE21"); // Pouting face
+		        
+		   
+
+	}
+
+}
